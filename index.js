ffc03aec4ac129b77f2dfb5ba7200ef4064f3571
niks-nikhil-anand <niks.anand.developer@gmail.com>
2024-02-14 17:28:48 +0530

    Done wih the server side implementation of socket.io

--- index.js
+++ index.js
@@ -9,13 +9,30 @@ const io = require("socket.io" , {
     }
 })
 
+// middleware
 app.use(cors())
 
 const PORT = process.env.port || 3000
 
-app.get("/" , (req,res) =>{
+
+app.get("/" , (_,res) =>{
     res.send("Server is running")
 })
 
+io.on('connection' , (socket)=>{
+    socket.emit('me' , socket.id)
+    socket.on('disconnect' , ()=>{
+        socket.broadcast.emit("callEnded")
+    })
+    socket.on("calluser" , ({userToCall , signalData , from , name}) =>{
+        io.to(userToCall).emit("calluser" , {signal :signal,from , name})
+    })
+    socket.on("answercall" , (data) =>{
+        io.to(data.to).emit("callaccepted" , data.signal)
+    })
+})
+
 server.listen(PORT , () => console.log(`server is listening on port ${PORT}`))
+
+
 
